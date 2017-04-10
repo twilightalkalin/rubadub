ef6d7322d39efa60def86452c614c0d24dc4d690
Ignatius Yudhistira <s3300020@student.rmit.edu.au>
2017-04-10 16:09:20 +1000

    add edit update show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,38 @@ def new
 	@article = Article.new
 end
 
+def index
+	@articles = Article.all
+end
+
+
+def edit
+@article=Article.find(params[:id])
+end
+
+def update 
+@article=Article.find(params[:id])
+	if @article.update(article_params)
+		flash[:notice] = " article was successfuly updated"
+	redirect_to article_path(@article)
+else
+	render 'edit'
+end
+end
+
+
+
+def show
+@article= Article.find(params[:id])
+
+end
+
 def create
 	#render plain: params[:article].inspect
 	@article= Article.new(article_params)
 	if @article.save
 		flash[:notice] = " article was successfuly created"
-	redirect_to articles_path(@article)
+	redirect_to article_path(@article)
 else
 	render 'new'
 end
